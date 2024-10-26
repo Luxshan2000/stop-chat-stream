b14fdc2590dab9623752ba61f3e7f0faa7c53290
Luxshan <luxshanlux2000@gmail.com>
2024-10-26 18:47:20 +0530

    fe setup added added

---
+++ fe/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Chat from './Chat';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <Chat />
+        </div>
+    );
+};
+
+export default App;
